e88b45317683e11b1777fe867c79ab8c1e9a4597
FinnHagan <2410686@dundee.ac.uk>
2023-03-20 18:17:21 +0000

    Level 1 Creation complete
    May need a tweak here and there but basically done

--- AC31009_Finn/Assets/Scripts/Platforms/LaunchPad.cs
+++ AC31009_Finn/Assets/Scripts/Platforms/LaunchPad.cs
@@ -3,13 +3,40 @@ using UnityEngine;
 public class LaunchPad : MonoBehaviour
 {
     [SerializeField] private float bounce;
+    [SerializeField] private float freezeTime;
+    private Animator anim;
+    private float timer;
+    private float originalAnimSpeed; // add this new variable
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
     {
-        if(collision.gameObject.CompareTag("Player"))
+        anim = GetComponent<Animator>();
+        originalAnimSpeed = anim.speed; // save the original animation speed
+    }
+
+    private void Update()
+    {
+        if (timer > 0f)
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                anim.Play("LaunchPlayer");
+                anim.speed = originalAnimSpeed; // reset the animation speed
+            }
+            else if (timer > 0f)
+            {
+                anim.speed = 0f;
+            }
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+            timer = freezeTime;
+        }
+    }
 }
